787eb94a4009ca8b3dd76d605fb44af87da5f9e9
Iclipsetop <iclipskii@gmail.com>
2020-03-04 17:05:13 +0100

    Initial commit

---
+++ src/IAPI.java
@@ -0,0 +1,20 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class IAPI extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+
+}
